5305bc3067af1f2e30a73d4e323f0bb0ff6d4a5b
Sebastian Amaro <sebastian.amaro90@gmail.com>
2018-09-16 23:28:11 -0300

    DB Setup Scripts

---
+++ DB Setup/2_Create Table Users.sql
@@ -0,0 +1,28 @@
+/****** Object:  Table [dbo].[Users]    Script Date: 09/16/2018 22:53:25 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](100) NOT NULL,
+	[LastName] [varchar](100) NOT NULL,
+	[Email] [varchar](255) NOT NULL,
+	[Password] [varchar](150) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
---
+++ DB Setup/3_Table population.sql
@@ -0,0 +1,14 @@
+INSERT INTO  Users 
+(FirstName, LastName, Email, Password)
+VALUES
+('Jonh','Doe','john@doe.com','pass1')
+
+INSERT INTO  Users 
+(FirstName, LastName, Email, Password)
+VALUES
+('Foo','Bar','foo@doe.com','pass2')
+
+INSERT INTO  Users 
+(FirstName, LastName, Email, Password)
+VALUES
+('Baz','Random','baz@doe.com','pass3')
